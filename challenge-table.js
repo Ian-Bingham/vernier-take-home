017084096b8dc0a2506b90c4baf872c25e50e9c7
Ian Bingham <ianbingham24@gmail.com>
2024-03-27 12:38:32 -0700

    fix: add point column in table; style table

--- challenge-table.js
+++ challenge-table.js
@@ -3,11 +3,44 @@ import { LitElement, html, css } from "lit";
 export class ChallengeTable extends LitElement {
 	static get styles() {
 		return css`
-			:host {
-				color: red;
+			.table-container {
+				margin-bottom: 4rem;
+				max-height: 300px;
+				overflow: auto;
+
+				border: 2px solid #838383;
 			}
+
 			table {
-				border: dashed purple;
+				width: 100%;
+				position: relative;
+
+				border-collapse: collapse;
+				text-align: center;
+			}
+
+			thead {
+				position: sticky;
+				top: 0;
+			}
+
+			thead tr,
+			tbody tr:not(:last-child) {
+				border-bottom: 1px solid #8c8c8c;
+			}
+
+			thead tr {
+				background: #b6b6b6;
+			}
+
+			th,
+			td {
+				padding: 8px 10px;
+			}
+
+			th:not(:last-child),
+			td:not(:last-child) {
+				border-right: 1px solid #8c8c8c;
 			}
 		`;
 	}
@@ -28,15 +61,17 @@ export class ChallengeTable extends LitElement {
 	}
 
 	renderHeader() {
-		return html`${this.headers.map((name) => {
-			return html` <th>${name}</th> `;
-		})} `;
+		return html`<th>Point</th>
+			${this.headers.map((name) => {
+				return html` <th>${name}</th> `;
+			})} `;
 	}
 
 	renderBody() {
-		return html`${this.data.map((col) => {
+		return html`${this.data.map((col, i) => {
 			return html`
 				<tr>
+					<td>${i + 1}</td>
 					<td>${col.x}</td>
 					<td>${col.y}</td>
 				</tr>
@@ -47,16 +82,18 @@ export class ChallengeTable extends LitElement {
 	render() {
 		return html`
 			<h1>${this.name}</h1>
-			<table>
-				<thead>
-					<tr>
-						${this.renderHeader()}
-					</tr>
-				</thead>
-				<tbody>
-					${this.renderBody()}
-				</tbody>
-			</table>
+			<div class="table-container">
+				<table>
+					<thead>
+						<tr>
+							${this.renderHeader()}
+						</tr>
+					</thead>
+					<tbody>
+						${this.renderBody()}
+					</tbody>
+				</table>
+			</div>
 		`;
 	}
 }
